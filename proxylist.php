09c0e266d4f11358dfeec3fed796ce936911544e
stormtrooper42 <faurest.lupine.pro@gmail.com>
2016-08-01 17:52:14 +0200

    ~ Proxylist

--- proxylist.php
+++ proxylist.php
@@ -3,4 +3,4 @@ require_once "class/class.gimmeproxy.php";
 $gp = new GimmeProxy();
 $gp->setReturnType(GIMME_PROXY_ASSOC_ARRAY);
 $gpResponse = $gp->gimmeProxy();
-echo "ip and port: " . $gpResponse['ip'] . "\n
+echo "ip and port: " . $gpResponse['ip'] . ""; 

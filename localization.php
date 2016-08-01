82ce9f2a22300804f12f78b5a3db0a4d42eb7588
stormtrooper42 <faurest.lupine.pro@gmail.com>
2016-08-01 07:31:52 +0200

    ~ Translate en_US

--- localization.php
+++ localization.php
@@ -9,6 +9,9 @@ require_once('lib/gettext/gettext.inc');
 $supported_locales = array('en_US', 'fr_FR');
 $encoding = 'UTF-8';
 
+if($_SERVER['HTTP_ACCEPT_LANGUAGE'] != "fr" || "fr_FR"){
+    $_GET['lang'] = 'en_US';
+}
 $locale = (isset($_GET['lang']))? $_GET['lang'] : DEFAULT_LOCALE;
 
 // gettext setup

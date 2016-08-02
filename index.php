339c31d3ce182f0c6bbd9df91b87a8431dc37948
stormtrooper42 <faurest.lupine.pro@gmail.com>
2016-08-02 21:07:34 +0200

    + Tweets

--- index.php
+++ index.php
@@ -1,6 +1,22 @@
 <?php
 include 'inc/header.php';
 require 'localization.php';
+require_once('lib/TwitterAPIExchange.php');
+$settings = array(
+	'oauth_access_token' => "YOUR_OAUTH_ACCESS_TOKEN",
+	'oauth_access_token_secret' => "YOUR_OAUTH_ACCESS_TOKEN_SECRET",
+	'consumer_key' => "YOUR_CONSUMER_KEY",
+	'consumer_secret' => "YOUR_CONSUMER_SECRET"
+);
+$url = "https://api.twitter.com/1.1/statuses/user_timeline.json";
+$requestMethod = "GET";
+$getfield = '?screen_name=VPNSox_Project&count=20';
+$twitter = new TwitterAPIExchange($settings);
+$string = json_decode($twitter->setGetfield($getfield)
+	->buildOauth($url, $requestMethod)
+	->performRequest(),$assoc = TRUE);
+//if($string["errors"][0]["message"] != "") {echo "<h3>Sorry, there was a problem.</h3><p>Twitter returned the following error message:</p><p><em>".$string[errors][0]["message"]."</em></p>";exit();}
+
 ?>
 	<div class="ip">
 		<div class="col-md-8">

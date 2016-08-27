4bd1cb055c438763ffa80cd845cb87a50ef9f2c3
root <root@packer-debian-8-amd64.digitalocean.com>
2016-08-27 10:48:19 -0400

    Fixes

---
+++ js/button.js
@@ -0,0 +1,9 @@
+function showBTC() {
+    document.getElementById("btc").style.display = "";
+    document.getElementById("vide").style.display = "none";
+} 
+    function hideBTC() {
+    document.getElementById("btc").style.display = "none";
+} 
+
+
--- old_site/README.md
+++
@@ -1,1 +0,0 @@
-VPNSox-Front
